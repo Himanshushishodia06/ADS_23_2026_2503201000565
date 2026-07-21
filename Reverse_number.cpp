6a726a19a456e6d16275cdff2a26a3ee256e8047
Himanshushishodia06 <himanshushishodia2007@gmail.com>
2026-07-21 14:54:16 +0530

    Array_Reversed

---
+++ Reverse_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num, revNum = 0, remainder;
+    cout << "Enter an integer: ";
+    cin >> num;
+    while(num != 0) {
+        remainder = num % 10;
+        revNum = revNum * 10 + remainder;
+        num /= 10;
+    }
+    cout << "Reversed Number: " << revNum << endl;
+    return 0;   
+    
+}
